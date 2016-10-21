6ba8dee4fb57a482dbcad26c5062d15a04a9c7d4
Michael Hendricks <michael@ndrix.org>
2016-10-21 11:27:17 -0400

    progress on conjunction

--- goal_conjunction.go
+++ goal_conjunction.go
@@ -1,12 +1,51 @@
-type GoalConjunction {
+type GoalConjunction struct {
 	Goals []Goal
+
+	choicepoints []choicepoint
+}
+
+type choicepoint struct {
+	index   int
+	barrier Barrier
 }
 
 func conjunction(goals ...Goal) Goal {
 	return &GoalConjunction{goals}
 }
 
-func (self *GoalConjunction) Next(c Context) (bool,bool) {
-	// TODO
-	return false, false
+func (self *GoalConjunction) Next(c Context) (bool, bool) {
+	goalIndex := 0
+	if len(choicepoints) > 0 {
+		p := choicepoints[len(choicepoints)-1]
+		self.choicepoints = self.choicepoints[0 : len(choicepoints)-1]
+		goalIndex = p.index
+		c.Trail().BacktrackTo(p.barrier)
+	}
+
+	for goalIndex < len(self.Goals) {
+		goal := self.Goals[goalIndex]
+		barrier := c.Trail().PushBarrier()
+		ok, more := goal.Next(c)
+		if ok {
+			if more {
+				// record a choicepoint
+				p := &choicepoint{goalIndex, barrier}
+				self.choicepoints = append(self.choicepoints, p)
+			}
+			goalIndex++
+			continue
+		} else {
+			// TODO backtrack to previous choicepoint
+		}
+	}
+
+	more := len(self.choicepoints) > 0
+	if !more {
+		for _, goal := range self.Goals {
+			if x, ok := goal.(NeedsCleanup); ok {
+				x.Cleanup()
+			}
+		}
+	}
+	return true, more
 }
