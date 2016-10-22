df6b23392961a72c5d861b225013164c9c410646
Michael Hendricks <michael@ndrix.org>
2016-10-22 15:35:31 -0400

    simple prolog tokenizer

---
+++ scanner.go
@@ -0,0 +1,370 @@
+package prolog
+
+import (
+	"fmt"
+	"io"
+)
+
+type Class int
+
+const (
+	Atom  Class = iota
+	Neck  Class = iota
+	Num   Class = iota
+	Punct Class = iota
+	Var   Class = iota
+)
+
+const EOF rune = -1
+
+type Position struct {
+	Filename string
+	Line     int
+	Column   int
+}
+
+func (pos Position) String() string {
+	f := pos.Filename
+	if f == "" {
+		f = "<input>"
+	}
+	return fmt.Sprintf("%s:%d:%d", f, pos.Line, pos.Column)
+}
+
+type SyntaxError struct {
+	Position Position
+	Message  string
+}
+
+func (err *SyntaxError) Error() string {
+	return fmt.Sprintf("%s: %s", err.Position, err.Message)
+}
+
+type Token struct {
+	Class    Class
+	Position Position
+	Text     string
+}
+
+func (t *Token) String() string {
+	switch t.Class {
+	case Atom:
+		return fmt.Sprintf("atom(%s)", t.Text)
+	case Neck:
+		return "neck"
+	case Num:
+		return fmt.Sprintf("num(%s)", t.Text)
+	case Punct:
+		return fmt.Sprintf("punct(%s)", t.Text)
+	case Var:
+		return fmt.Sprintf("var(%s)", t.Text)
+	default:
+		return "<unknown token class>"
+	}
+}
+
+type Scanner struct {
+	r io.RuneScanner
+
+	filename string
+	err      *SyntaxError
+
+	line       int
+	column     int
+	prevLine   int // before calling next()
+	prevColumn int // before calling next()
+}
+
+func NewScanner(r io.RuneScanner) *Scanner {
+	s := &Scanner{
+		r:      r,
+		line:   1,
+		column: 0,
+	}
+	return s
+}
+
+func (s *Scanner) Scan() (*Token, error) {
+	var ch rune
+	for {
+		ch = s.peek()
+		if ch == EOF {
+			if s.err == nil {
+				return nil, io.EOF
+			}
+			return nil, s.err
+		}
+
+		if ch >= 'a' && ch <= 'z' { // atom
+			return s.scanAtom()
+		} else if ch >= 'A' && ch <= 'Z' { // variable
+			return s.scanVariable()
+		} else if ch >= '0' && ch <= '9' { // number
+			return s.scanNumber()
+		} else if ch == ':' { // neck
+			return s.scanNeck()
+		} else if ch == ' ' || ch == '\n' { // white space
+			s.skipSpace()
+			continue
+		} else {
+			break
+		}
+	}
+
+	_ = s.next() // consume punctuation character
+	t := &Token{
+		Class:    Punct,
+		Position: s.pos(),
+		Text:     string([]rune{ch}),
+	}
+	return t, nil
+}
+
+func (s *Scanner) peek() rune {
+	ch := s.next()
+	if ch == EOF {
+		return ch
+	}
+	s.back()
+	return ch
+}
+
+func (s *Scanner) back() {
+	err := s.r.UnreadRune()
+	if err != nil {
+		panic(err)
+	}
+
+	s.line = s.prevLine
+	s.column = s.prevColumn
+}
+
+// consumes the next character in the stream. stores errors in s.err
+func (s *Scanner) next() rune {
+	s.prevLine = s.line
+	s.prevColumn = s.column
+
+	ch, _, err := s.r.ReadRune()
+	if err == io.EOF {
+		return EOF
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	// handle prohibited characters
+	if ch == '\t' || ch == '\r' {
+		s.err = s.prohibitedCharacter(ch)
+		return EOF
+	}
+
+	// update position information
+	if ch == '\n' {
+		s.line++
+		s.column = 0
+	} else {
+		s.column++
+	}
+
+	return ch
+}
+
+func (s *Scanner) prohibitedCharacter(ch rune) *SyntaxError {
+	var name string
+	switch ch {
+	case '\t':
+		name = "tab"
+	case '\r':
+		name = "carriage return"
+	}
+	return &SyntaxError{
+		Position: s.pos(),
+		Message:  fmt.Sprintf("The %s character is prohibited", name),
+	}
+}
+
+func (s *Scanner) pos() Position {
+	return Position{
+		Filename: s.filename,
+		Line:     s.line,
+		Column:   s.column,
+	}
+}
+
+func (s *Scanner) skipSpace() {
+	for {
+		ch := s.next()
+		if ch == ' ' || ch == '\n' {
+			continue
+		}
+		s.back()
+		break
+	}
+}
+
+func (s *Scanner) scanAtom() (*Token, error) {
+	chars := make([]rune, 0)
+
+	ch := s.next()
+	pos := s.pos()
+CH:
+	for {
+		switch {
+		case ch >= 'a' && ch <= 'z', ch == '_':
+			chars = append(chars, ch)
+		case ch == '(', ch == ')', ch == ',', ch == '.', ch == ' ':
+			s.back()
+			break CH
+		case ch == EOF:
+			break CH
+		default:
+			err := &SyntaxError{
+				Position: s.pos(),
+				Message:  fmt.Sprintf("Unexpected atom character: %c", ch),
+			}
+			return nil, err
+		}
+
+		ch = s.next()
+	}
+
+	t := &Token{
+		Class:    Atom,
+		Position: pos,
+		Text:     string(chars),
+	}
+	return t, nil
+}
+
+func (s *Scanner) scanNeck() (*Token, error) {
+	pos := s.pos()
+	ch := s.next()
+	if ch == ':' {
+		ch = s.next()
+		if ch == '-' {
+			t := &Token{
+				Class:    Neck,
+				Position: pos,
+				Text:     ":-",
+			}
+			return t, nil
+		}
+	}
+
+	err := &SyntaxError{
+		Position: pos,
+		Message:  "Expected neck (:-)",
+	}
+	return nil, err
+}
+
+func (s *Scanner) scanVariable() (*Token, error) {
+	chars := make([]rune, 0)
+
+	ch := s.next()
+	pos := s.pos()
+CH:
+	for {
+		switch {
+		case ch >= 'A' && ch <= 'Z':
+			if len(chars) > 0 {
+				prev := chars[len(chars)-1]
+				if prev < 'a' || prev > 'z' {
+					err := &SyntaxError{
+						Position: s.pos(),
+						Message:  fmt.Sprintf("variable names may not have consecutive uppercase letters, got %c", ch),
+					}
+					return nil, err
+				}
+			}
+			chars = append(chars, ch)
+		case ch >= 'a' && ch <= 'z':
+			if len(chars) == 0 {
+				panic("called scanVariable without upper case letter next in stream")
+			}
+			chars = append(chars, ch)
+		case ch == '(', ch == ')', ch == ',', ch == '.':
+			s.back()
+			break CH
+		case ch == EOF:
+			break CH
+		default:
+			err := &SyntaxError{
+				Position: s.pos(),
+				Message:  fmt.Sprintf("Unexpected variable character: %c", ch),
+			}
+			return nil, err
+		}
+
+		ch = s.next()
+	}
+
+	t := &Token{
+		Class:    Var,
+		Position: pos,
+		Text:     string(chars),
+	}
+	return t, nil
+}
+
+func (s *Scanner) scanNumber() (*Token, error) {
+	x, err := s.scanInteger()
+	if err != nil {
+		return nil, err
+	}
+
+	switch ch := s.next(); ch {
+	case '.':
+		y, err := s.scanInteger()
+		if err != nil {
+			return nil, err
+		}
+		t := &Token{
+			Class:    Num,
+			Position: x.Position,
+			Text:     x.Text + "." + y.Text,
+		}
+		return t, nil
+	default:
+		t := &Token{
+			Class:    Num,
+			Position: x.Position,
+			Text:     x.Text,
+		}
+		return t, nil
+	}
+}
+
+func (s *Scanner) scanInteger() (*Token, error) {
+	chars := make([]rune, 0)
+
+	ch := s.next()
+	pos := s.pos()
+CH:
+	for {
+		switch {
+		case ch >= '0' && ch <= '9', ch == '_':
+			chars = append(chars, ch)
+		case ch == '(', ch == ')', ch == ',', ch == '.':
+			s.back()
+			break CH
+		case ch == EOF:
+			break CH
+		default:
+			err := &SyntaxError{
+				Position: s.pos(),
+				Message:  fmt.Sprintf("Unexpected number character: %c", ch),
+			}
+			return nil, err
+		}
+
+		ch = s.next()
+	}
+
+	t := &Token{
+		Class:    Num, // not really
+		Position: pos,
+		Text:     string(chars),
+	}
+	return t, nil
+}
---
+++ scanner_test.go
@@ -0,0 +1,48 @@
+package prolog
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestScanner(t *testing.T) {
+	tests := map[string]string{
+		`hello.`: `[atom(hello) punct(.)]`,
+		`3.141.`: `[num(3.141) punct(.)]`,
+		`X`:      `[var(X)]`,
+		`FooBar`: `[var(FooBar)]`,
+
+		"main :-\n    hello.": `[atom(main) neck atom(hello) punct(.)]`,
+	}
+	for prolog, expected := range tests {
+		ts, err := tokens(prolog)
+		if err != nil {
+			t.Errorf("oops: %s", err)
+			return
+		}
+		got := fmt.Sprintf("%s", ts)
+		if got != expected {
+			t.Errorf("got : %s\nwant: %s\n", got, expected)
+		}
+	}
+}
+
+func tokens(text string) ([]*Token, error) {
+	ts := make([]*Token, 0)
+
+	s := NewScanner(strings.NewReader(text))
+	for {
+		t, err := s.Scan()
+		if err == io.EOF {
+			return ts, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		ts = append(ts, t)
+	}
+
+	return nil, nil
+}
