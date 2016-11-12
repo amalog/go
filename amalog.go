b15d1dda2fba6bae44bd2bcdb5037d7b03ed9290
Michael Hendricks <michael@ndrix.org>
2016-11-12 10:46:20 -0500

    ama: format given source code
    For now invoking 'ama foo.ama' reads the terms from 'foo.ama' and writes
    them to stdout using canonical formatting.  This is helpful for testing
    the parser and formatted output.

--- amalog.go
+++ amalog.go
@@ -1,7 +1,11 @@
 package amalog // import "github.com/amalog/go"
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"os"
+
+	"github.com/amalog/go/term"
 )
 
 type Amalog struct {
@@ -10,6 +14,38 @@ type Amalog struct {
 }
 
 func (ama *Amalog) Run(args []string) int {
-	fmt.Fprintf(ama.Out, "TODO implement Amalog\n")
-	return 1
+	if len(args) == 0 {
+		fmt.Fprintf(ama.Err, "Usage: ama foo.ama\n")
+		return 1
+	}
+
+	return ama.CmdFormat(args[0])
+}
+
+func (ama *Amalog) CmdFormat(filename string) int {
+	// open Amalog source code file
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(ama.Err, "%s\n", err)
+		return 1
+	}
+	buf := bufio.NewReader(file)
+
+	// read and output terms
+	style := term.Style{}
+	reader := term.NewReader(buf)
+	for {
+		t, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(ama.Err, "read: %s", err)
+			return 1
+		}
+
+		t.Format(ama.Out, style)
+	}
+
+	return 0
 }
