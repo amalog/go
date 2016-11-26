48aa1742b6cb0331c8c42f160490b6042106df4a
Michael Hendricks <michael@ndrix.org>
2016-11-26 14:25:35 -0500

    Implement module search path
    When importing Amalog modules, we search inside any "amalog_modules"
    directory contained within a parent of the directory that contains the
    source code or the executable.
    
    This allows users to have Amalog modules that are specific to a project
    or a user or the entire system.  The most specific directory takes
    precedence.
    
    Many languages choose this search path based on an environment
    variable.  In my experience that leads to a lot of confusion and
    requires extra work to configure the environment.  One has to create a
    directory structure for the module source code anyway, so we might as
    well establish a convention and rely on that.

--- machine.go
+++ machine.go
@@ -2,6 +2,7 @@ package amalog // import "github.com/amalog/go"
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/amalog/go/term"
 )
@@ -17,19 +18,66 @@ func NewMachine() *Machine {
 // LoadRoot populates the machine's root term with the contents of a file
 // and loads all of that term's module dependencies.
 func (m *Machine) LoadRoot(filename string) error {
+	// read root term from file
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
-
 	m.root, err = term.ReadAll(file)
 	if err != nil {
 		return err
 	}
+
+	// find directories to search for modules
+	modulePath, err := m.modulePath(filename)
+	if err != nil {
+		return err
+	}
+	_ = modulePath
 
 	return nil
 }
 
 func (m *Machine) Call(name string, args ...term.Term) *Result {
 	return nil
+}
+
+// modulePath returns a list of directories within which we should search for
+// modules. The directories returned are from highest to lowest priority.  All
+// directories are guaranteed to exist in the filesystem at the time this method
+// was invoked.
+//
+// The argument should be the path to source code for the root term.
+func (m *Machine) modulePath(src string) ([]string, error) {
+	// start search at directories holding source code and executable
+	starts := make([]string, 0, 2)
+	for _, p := range []string{src, os.Args[0]} {
+		p, err := filepath.Abs(p)
+		if err != nil {
+			return nil, err
+		}
+		starts = append(starts, filepath.Dir(p))
+	}
+
+	// walk back to the root looking for module directories
+	paths := make([]string, 0)
+	tried := make(map[string]bool)
+	for _, p := range starts {
+		for {
+			tried[p] = true
+			candidate := filepath.Join(p, "amalog_modules")
+			if stat, err := os.Stat(candidate); err == nil && stat.IsDir() {
+				paths = append(paths, candidate)
+			} else if err != nil && !os.IsNotExist(err) {
+				return nil, err // unexpected error
+			}
+
+			p = filepath.Dir(p)
+			if tried[p] {
+				break
+			}
+		}
+	}
+
+	return paths, nil
 }
